9549d175e27dd861ab91c9d519af46176231a58b
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2023-03-25 11:03:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 # 删除老argon
 rm -rf package/lean/luci-theme-argon
 # 拉取argon主题
