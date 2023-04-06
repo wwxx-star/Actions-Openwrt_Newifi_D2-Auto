19bfd7ec49502e8ef304acd32d4194f597850531
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2023-04-06 18:47:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,5 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 #git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 #git clone  https://github.com/leshanydy2022/luci-app-smartdns  package/luci-app-smartdns 
-git clone  https://github.com/kenzok8/openwrt-packages.git  package/kenzo
-git clone  https://github.com/kenzok8/small.git  package/small
-git clone  https://github.com/sbwml/luci-app-mosdns.git  package/luci-app-mosdns
+git clone  https://github.com/NueXini/NueXini_Packages.git  package/NueXini_Packages
 
