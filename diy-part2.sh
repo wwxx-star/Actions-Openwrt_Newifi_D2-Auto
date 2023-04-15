f28726a953dd2487f9cc14a51036208ce980fdff
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2023-04-15 08:34:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 # 删除老argon
-rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
 # 拉取argon主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/xiaorouji/openwrt-passwall2.git  package/luci-app-passwall2
 git clone  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
 git clone -b luci  https://github.com/xiaorouji/openwrt-passwall.git  package/luci-app-passwall
