e913e28fdbcffd6485192c3b25a2ee1d629ff22b
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2023-10-14 07:09:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,13 +19,11 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 # # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 # # git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 # git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 # #git clone https://github.com/Leslie-Wong/luci-app-passwall.git package/passwall/luci/luci-app-passwall
 # #git clone https://github.com/Yangself666/luci-app-passwall.git package/passwall/luci
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git  package/luci-app-adguardhome 
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# #git clone https://github.com/wwxx-star/openwrt-packages1  package/openwrt-packages
+# #git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git  package/luci-app-adguardhome 
+# #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/wwxx-star/openwrt-packages1  package/openwrt-packages
 # 拉取微信推送、京东签到插件
 # git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 #git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
